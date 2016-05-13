16d7c61f6e9e184188db9c36d1fb8773c382e15e
waflessnet <francisco@twpanel.com>
2016-05-13 04:18:17 +0000

    correcciondes de directorio

--- install.sh
+++ install.sh
@@ -2,21 +2,22 @@
 #autor: Francisco Céspedes.
 #email: ncwapuntes@gmail.com
 D=$(pwd);
-B="~./vim/bundle/"
 echo "creando directorios para vim"
-mkdir -p ~/.vim/{bundle,autoload,backup_files,colors,swap_files,undo_files}
+mkdir -p $HOME/.vim/{bundle,autoload,backup_files,colors,swap_files,undo_files}
 echo "Instalando pathogen"
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "Instalando bundles"
-grep -v '^#' bundles.txt| while read p; do
-	git clone $p $B
+cd $HOME && cd ./.vim/bundle
+pwd
+grep -v '^#' $D/bundles.txt| while read p; do
+	git clone $p  
 done
 echo "Bundles instalados \n "
 echo "Copiamos colores"
 cd $D
-cp colores/*.vim ~/.vim/colors/
+cp colors/*.vim $HOME/.vim/colors/
 echo "Copia de vimrc a .vimrc"
-cp vimrc ~/.vimrc
-clear
+cp vimrc $HOME/.vimrc
+vim -c :Helptags -c :qall
 echo "Vim waflessnet instalado."
 
